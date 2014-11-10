1ddafa26da13502df67f5490574deb273df27666
Hassan Althobaiti <hmthobaiti@gmail.com>
2014-11-10 02:01:41 -0500

    The seed file has been populated with some data. Please seed

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ActiveRecord::Base
   belongs_to :category
   has_many :item_requests, dependent: :destroy
 
-  validates :name, :description, :image, :price, presence: true
+  validates :name, :description, :price, presence: true
 
   mount_uploader :image, ImageUploader
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,39 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Category.destroy_all
-Category.create(category_name: 'Tools')
-Category.create(category_name: 'Toys')
-Category.create(category_name: 'Gardening Items')
-Category.create(category_name: 'Others')
+Category.create(id: '1', category_name: 'Tools')
+Category.create(id: '2', category_name: 'Toys')
+Category.create(id: '3', category_name: 'Gardening Items')
+Category.create(id: '4', category_name: 'Others')
+
+Item.destroy_all
+
+Item.new(:id => '1', :name => 'Drill', :description => 'Mint condition. Like new.',
+         :remote_image_url => "http://toolmonger.com/wp-content/uploads/2006/05/standarddrill.jpg",
+         :price => 45.99, :keywords => 'tools, drill',
+         :category_id => '1', :user_id => '1').save(:validate => false)
+
+
+Item.new(:id => '2', :name => 'Another Drill', :description => 'Brand new. Still in the original box.',
+         :remote_image_url => "http://cdn.dice.com/wp-content/uploads/2012/08/shutterstock_74316340.jpg",
+         :price => '65', :keywords => 'tools, drill',
+         :category_id => '1', :user_id => '1').save(:validate => false)
+
+
+Item.new(:id => '3', :name => 'Screwdriver', :description => 'Used but in a good condition.',
+         :remote_image_url => "http://www.clipartlord.com/wp-content/uploads/2013/04/screwdriver.png",
+         :price => 19, :keywords => 'tools, screwdriver',
+         :category_id => '1', :user_id => '1').save(:validate => false)
+
+Item.new(:id => '4', :name => 'Toy', :description => 'Very useful if you have a child.',
+         :remote_image_url => "http://www.frenchblossom.com/1314-2620-thickbox/bird-of-paradise-pull-along-toy.jpg",
+         :price => 8.99, :keywords => 'toys, fun',
+         :category_id => '2', :user_id => '1').save(:validate => false)
+
+Item.new(:id => '5', :name => 'Bike', :description => 'This bike will make crazy.',
+         :remote_image_url => "http://salsacycles.com/files/bikes/bikes_Mukluk2_2013_2.jpg",
+         :price => 145.45, :keywords => 'bike, workout',
+         :category_id => '4', :user_id => '1').save(:validate => false)
+
+
+puts "Success: The data has been successfully populated."
