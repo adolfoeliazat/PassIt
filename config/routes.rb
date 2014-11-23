c552006d08710eb39999a49e7f890b75bae86499
Hassan Althobaiti <hmthobaiti@gmail.com>
2014-11-23 04:46:30 -0500

    Users now can add reviews

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :items
+  post 'reviews/create'
   root to: "spage#welcome"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
